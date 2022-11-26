172c48cc2e292e6affaa4c5eb6cd02194ee863fd
igr1 <dfrgth>
2022-11-26 17:15:40 +1000

    5.4 practica

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static boolean isPalindrome(String text) {
+        for (int i = 0; i < text.length() / 2; i++) {
+            if (text.charAt(i) != text.charAt(text.length() - i - 1)) return false;
+        }
+        return true;
+    }
+    public static boolean checkPalindrome(String s) {
+        String text = new StringBuilder(s).reverse().toString();
+        return text.equals(s);
+    }
 }
